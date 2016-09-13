176407b14faf6fa92abb998cabee670d904e3d14
Jirapong Rungsawang <newsonesnsd@gmail.com>
2016-09-13 19:07:19 +0700

    homework4

--- chapter5/Homework4.java
+++ chapter5/Homework4.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Homework4 {
     public static void main(String[] args) {
-
+        int randomNum = (int) (Math.random()*900)+100; // n2 = 999, n1 = 100
+        System.out.println("Random num is : " + randomNum);
     }
 }
