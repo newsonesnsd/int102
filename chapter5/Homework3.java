210abb6f723f900b618c27dbadd7374b567ba35f
Jirapong Rungsawang <newsonesnsd@gmail.com>
2016-09-13 18:50:53 +0700

    add Input

--- chapter5/Homework3.java
+++ chapter5/Homework3.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Homework3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print();
+        System.out.print("Enter Num : ");
+        int getNum = input.nextInt();
+        
     }
 }
