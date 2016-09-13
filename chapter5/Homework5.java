0a67af9a9ee31e225797b306eb0e10cf89987473
Jirapong Rungsawang <newsonesnsd@gmail.com>
2016-09-13 21:25:01 +0700

    add loop lift

--- chapter5/Homework5.java
+++ chapter5/Homework5.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class Homework5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+        int liftFloor = 1;
+        System.out.print("Enter Floor : ");
+        int getFloor = input.nextInt();
+        do {
+            System.out.print(getFloor);
+            getFloor++;
+        } while (liftFloor<=getFloor);
     }
 }
