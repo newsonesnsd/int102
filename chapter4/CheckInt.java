2ce5e93ca76e76f27a0c6270c3b0e26652a6192b
Jirapong Rungsawang <newsonesnsd@users.noreply.github.com>
2016-09-22 22:45:24 +0700

    PreMidterm 1
    Pre Midterm Exam by P'Tei 
    Check integer to collect in type of variable

---
+++ chapter4/CheckInt.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CheckInt {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Create VARIABLE for min max value of data type
+        final long byteMin = Byte.MIN_VALUE;
+        final long byteMax = Byte.MAX_VALUE;
+        final long shortMin = Short.MIN_VALUE;
+        final long shortMax = Short.MAX_VALUE;
+        final long intMin = Integer.MIN_VALUE;
+        final long intMax = Integer.MAX_VALUE;
+        final long longMin = Long.MIN_VALUE;
+        final long longMax = Long.MAX_VALUE;
+        String typeOfInt = "";
+        System.out.print("Enter your integer : ");
+        Long getNum = input.nextLong();
+        System.out.println(shortMin);
+        System.out.println(shortMax);
+        if (getNum >=longMin && getNum <= longMax) {
+            typeOfInt += "long";
+        }
+        if (getNum >= intMin && getNum <= intMax) {
+            typeOfInt += ", int";
+        }
+        if (getNum >= shortMin && getNum <= shortMax) {
+            typeOfInt += ", short";
+        }
+        if (getNum >= byteMin && getNum <= byteMax) {
+            typeOfInt += ", byte";
+        }
+        System.out.println(typeOfInt);
+    }
+}
