768cc9cc96f2f0c08b6224118cf1038647526618
Jirapong Rungsawang <newsonesnsd@gmail.com>
2016-09-23 11:20:40 +0700

    PreMidterm2 Random Grade

--- chapter4/PreMidterm2.java
+++ chapter4/PreMidterm2.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class PreMidterm2 {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        // Random number
+        int randomGrade = (int) (Math.Random()*5) + 1;
+        String grade = "";
+        switch (randomGrade) {
+            case 1:
+                grade = "A";
+                break;
+            case 2:
+                grade = "B";
+                break;
+            case 3:
+                grade = "C";
+                break;
+            case 4:
+                grade = "D";
+            case 5:
+                grade = "F";
+                break;
+        }
+        System.out.print(grade);
     }
 }
