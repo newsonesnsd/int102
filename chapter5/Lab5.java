51962697aa9a7e3ec37b321f226cd92185d6c47c
Jirapong Rungsawang <newsonesnsd@users.noreply.github.com>
2016-10-03 11:24:37 +0700

    Lift

---
+++ chapter5/Lab5.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Lab5 {
+	public static void main(String[] args) {
+		int liftFloor =1;
+		int liftGo = 0;
+		Scanner input = new Scanner (System.in);
+		while (true) {
+			System.out.print("Enter Floor ");
+			liftGo = input.nextInt();
+			if(liftGo>=1 && liftGo<=5) {
+				System.out.print("Elevator ");
+				if (liftFloor <= liftGo) {
+					while(liftFloor <= liftGo) {
+						System.out.print(liftFloor + ".. ");
+						liftFloor++;
+					}
+					System.out.println("");
+					System.out.println("Now " + (--liftFloor) + " Floor");
+				}
+				else {
+					while(liftFloor > liftGo) {
+						System.out.print(liftFloor + ".. ");
+						liftFloor--;
+					}
+					System.out.print(liftFloor + ".. ");
+					System.out.println("");
+					System.out.println("Now " + liftFloor + " Floor");
+				}
+			}
+			else {
+				System.out.println("Wrong Floor");
+			}
+		}
+	}
+}
