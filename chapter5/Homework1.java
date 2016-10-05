1711690af40de99b035f2356f9b2a3d8ff5de996
Jirapong Rungsawang <newsonesnsd@gmail.com>
2016-10-05 08:34:06 +0700

    Edit lottery

--- chapter5/Homework1.java
+++ chapter5/Homework1.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Homework1 {
     public static void main(String[] args) {
@@ -6,20 +6,22 @@ public class Homework1 {
         int randomLotteryNum = (int) (Math.random() * 10);
         String lotteryNum = "";
         int i = 1;
-
         while (i<=10) {
             randomLotteryNum = (int) (Math.random() * 10);
             lotteryNum += randomLotteryNum;
             i++;
         }
         int numLength = lotteryNum.length();
-        System.out.println(lotteryNum + " nee");
+        System.out.println(lotteryNum + " is reward");
         // reward
         String allDigit = lotteryNum;
         String lastTwoDigit = lotteryNum.substring(numLength-2);
         String lastThreeDigit = lotteryNum.substring(numLength-3);
         System.out.print("Enter lottery : ");
         String getNum = input.nextLine();
+        // Check input
+        String getTwoDigit = getNum.substring(numLength-2);
+        String getThreeDigit = getNum.substring(numLength-3);
         while (getNum.equals("0") == false) {
             if (getNum.equals(allDigit)) {
                 System.out.println("all digits!");
