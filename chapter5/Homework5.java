064d0131d4e2bda8f9eb46342d470912b73b31ed
Jirapong Rungsawang <newsonesnsd@gmail.com>
2016-10-05 08:08:02 +0700

    Edit variable

--- chapter5/Homework5.java
+++ chapter5/Homework5.java
@@ -1,14 +1,36 @@
 import java.util.Scanner;
 
 public class Homework5 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int liftFloor = 1;
-        System.out.print("Enter Floor : ");
-        int getFloor = input.nextInt();
-        do {
-            System.out.print(liftFloor + "... ");
-            liftFloor++;
-        } while (liftFloor<=getFloor);
-    }
+	public static void main(String[] args) {
+		int liftFloor =1;
+		int liftGo = 0;
+		Scanner input = new Scanner (System.in);
+		while (true) {
+			System.out.print("Enter Floor ");
+			liftGo = input.nextInt();
+			if(liftGo>=1 && liftGo<=5) {
+				System.out.print("Elevator ");
+				if (liftFloor <= liftGo) {
+					while(liftFloor <= liftGo) {
+						System.out.print(liftFloor + ".. ");
+						++liftFloor;
+					}
+					System.out.println("");
+					System.out.println("Now " + liftFloor + " Floor");
+				}
+				else {
+					while(liftFloor > liftGo) {
+						System.out.print(liftFloor + ".. ");
+						liftFloor--;
+					}
+					System.out.print(liftFloor + ".. ");
+					System.out.println("");
+					System.out.println("Now " + liftFloor + " Floor");
+				}
+			}
+			else {
+				System.out.println("Wrong Floor");
+			}
+		}
+	}
 }
