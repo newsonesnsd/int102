683b694bc39a9c0571071dca79ca4a54f5429cca
Jirapong Rungsawang <newsonesnsd@users.noreply.github.com>
2016-10-10 10:11:34 +0700

    Beautifull Code

--- chapter5/Inclass1.java
+++ chapter5/Inclass1.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class Inclass1 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter your sentence : ");
-		String getText = input.nextLine();
-		System.out.print("Enter find word : ");
-		String getFindWord = input.nextLine();
 		int count = 0;
+		int searchWord = 0;
+		System.out.print("Enter your sentence : ");
+		String getText, getFindWord;
+		getText = input.nextLine();
+		System.out.print("Enter find word : ");
+		getFindWord = input.nextLine();
 		System.out.print("Found '" + getFindWord + "' at position " );
-		int serchWord = getText.indexOf(getFindWord, count);
-		while (serchWord != -1) {
-			serchWord = getText.indexOf(getFindWord, count);
-			if (serchWord != -1) {
-				System.out.print(serchWord+1 + " ");
-				count +=serchWord+1;
-				//continue;
+		searchWord = getText.indexOf(getFindWord, count);
+		while (searchWord != -1) {
+			searchWord = getText.indexOf(getFindWord, count);
+			if (searchWord != -1) {
+				System.out.print(searchWord+1 + " ");
+				count +=searchWord+1;
 			}	
 		}
 		
