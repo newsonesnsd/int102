59340cae01cba39905da633b3bfeb393f73738e3
Jirapong Rungsawang <newsonesnsd@gmail.com>
2016-10-31 00:18:42 +0700

    edit bug

--- chapter6/TestStudent.java
+++ chapter6/TestStudent.java
@@ -5,7 +5,7 @@ public class TestStudent {
         std1.setStudentName("Jirapong");
         String studentName = std1.getStudentName();
         std1.setStudentID();
-        long studentID = std1.getStudentID();
+        String studentID = std1.getStudentID();
         std1.setStudentSec();
         String studentSec = std1.getStudentSec();
 
