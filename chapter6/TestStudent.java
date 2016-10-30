87d4ff3b3f1a37b0d0c465ed67463d0044ccf90b
Jirapong Rungsawang <newsonesnsd@gmail.com>
2016-10-31 00:53:20 +0700

    add method

--- chapter6/TestStudent.java
+++ chapter6/TestStudent.java
@@ -4,13 +4,17 @@ public class TestStudent {
         Student std1 = new Student();
         std1.setStudentName("Jirapong");
         String studentName = std1.getStudentName();
-        long stdID = std1.studentID();
+        long stdID = std1.Id();
         String stdSec = std1.sec();
         String stdMail = std1.mail();
         std1.setSubject("INT102");
         String subject = std1.getSubject();
+        System.out.println(studentName);
+        System.out.println("ID : " + stdID);
+        System.out.println(stdSec);
+        System.out.println("Mail : " + stdMail);
         std1.drop(subject);
-
+        String stdStatus = std1.status();
 
     }
 }
