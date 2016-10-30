6c25efbcd52a9b66757dd5da99afca9f469a5229
Jirapong Rungsawang <newsonesnsd@gmail.com>
2016-10-31 00:10:57 +0700

    add Homework

---
+++ chapter6/Student.java
@@ -0,0 +1,59 @@
+public class Student {
+    private String studentName, learnSubject, studentSec, subjectStatus;
+    private long studentID;
+    private boolean subjectDrop = false;
+
+    public void setStudentName(String name) {
+        studentName = name;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentID(long id) {
+        studentID = (long) (Math.random()*(59130500143L-59130500001L+1))+59130500001L;
+    }
+
+    public String getStudentID() {
+        return "Your ID is : " + studentID;
+    }
+
+    public void setStudentSec(long studentID){
+        if (studentID <= (studentID / 2)) {
+            studentSec = "A";
+        }
+        else {
+            studentSec = "B";
+        }
+    }
+
+    public String getStudentSec() {
+        return "Your sec is : " + studentSec;
+    }
+
+    public void setSubject(String subject) {
+        learnSubject = subject;
+    }
+
+    public String getSubject() {
+        return learnSubject;
+    }
+
+    public boolean drop(String learnSubject) {
+        subjectDrop = true;
+        return subjectDrop;
+    }
+
+
+    public String status() {
+        if (subjectDrop == true) {
+            subjectStatus = learnSubject + " was dropped";
+        }
+        else {
+            subjectStatus = learnSubject + " now lerning";
+        }
+        return subjectStatus;
+    }
+
+}
---
+++ chapter6/TestStudent.java
@@ -0,0 +1,10 @@
+public class TestStudent {
+
+    public static void main(String[] args) {
+        Student std1 = new Student();
+        std1.setStudentName("Jirapong");
+        String studentName = std1.getStudentName();
+
+    }
+
+}
